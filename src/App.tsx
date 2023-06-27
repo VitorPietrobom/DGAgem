4f89248cb89310a147c0f79c5ada763843017f12
Vitor Rodrigues Pietrobom <v245584@dac.unicamp.br>
2023-06-27 02:05:45 -0300

    Add routing and correct folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,13 @@
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { ReactElement, useEffect, useState } from 'react';
-import { Theme } from '@mui/material/styles/createTheme';
-import { Home } from './components/home/home';
-
-const getThemePallete = (isOnDarkMode: boolean): Theme => {
-  return createTheme({
-    palette: {
-      mode: isOnDarkMode ? "dark" : "light"
-    },
-    components: {
-      MuiCard: {
-        styleOverrides: {
-          root: {
-            borderRadius: "20px"
-          }
-        }
-      }
-    }
-  });
-}
+import { ReactElement } from 'react';
+import { Home } from './pages/Home/home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export const DGAViagensApp = (): ReactElement => {
-  const lightThemePallete = getThemePallete(false);
-  const [dgaViagensAppTheme, setDGAViagensAppTheme] = useState(lightThemePallete);
-  
-  const defineThemePallete = (event: any) => {
-    const isOnDarkMode = event.matches;
-    const theme = getThemePallete(isOnDarkMode);
-    setDGAViagensAppTheme(theme);
-  }
-
-  useEffect(() => {
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => defineThemePallete(event));
-    const isOnDarkMode =  window.matchMedia('(prefers-color-scheme: dark)').matches;
-    if (isOnDarkMode) {
-      const darkThemePallete = getThemePallete(isOnDarkMode);
-      setDGAViagensAppTheme(darkThemePallete);
-    }
-  }, []);
-
   return (
-    <ThemeProvider theme={dgaViagensAppTheme}>
-      <CssBaseline />
-      <Home />
-    </ThemeProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+    </Router>
   )
 }
